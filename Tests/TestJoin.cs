23133b2690df62b3629bfd48f94d72b5febe6236
Petr Janik <peta.janik@email.cz>
2021-07-13 09:17:39 +0200

    Add more tests for Join()

--- Tests/TestJoin.cs
+++ Tests/TestJoin.cs
@@ -95,10 +95,10 @@ namespace Tests
 
             var dataRight = new[]
             {
-                new object[] {"Carlos", "D1",},
-                new object[] {"Tatiana", "D2",},
-                new object[] {"Henry", "D3",},
-                new object[] {"Gordon", "D4",},
+                new object[] {"Carlos", "D1"},
+                new object[] {"Tatiana", "D2"},
+                new object[] {"Henry", "D3"},
+                new object[] {"Gordon", "D4"},
             };
 
             var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
@@ -152,10 +152,10 @@ namespace Tests
 
             var dataRight = new[]
             {
-                new object[] {"Carlos", "D1",},
-                new object[] {"Tatiana", "D2",},
-                new object[] {"Henry", "D3",},
-                new object[] {"Gordon", "D4",},
+                new object[] {"Carlos", "D1"},
+                new object[] {"Tatiana", "D2"},
+                new object[] {"Henry", "D3"},
+                new object[] {"Gordon", "D4"},
             };
 
             var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
@@ -227,6 +227,262 @@ namespace Tests
                 new object[] {"Tatiana", "B2", "C2", "D2", "Tatiana"},
                 new object[] {"Tatiana", "B2", "C2", "D4", "Tatiana"},
             };
+
+            var expectedColumns = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            Assert.AreEqual(expectedData, joinedTable.Data);
+            Assert.AreEqual(expectedColumns, joinedTable.Columns);
+        }
+
+        [Test]
+        public void JoinWithMultipleInLeftTable()
+        {
+            var columnsLeft = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+            };
+
+            var columnsRight = new List<string>
+            {
+                "D",
+                "E",
+            };
+
+            var dataLeft = new[]
+            {
+                new object[] {"Carlos", "B1", "C1"},
+                new object[] {"Tatiana", "B2", "C2"},
+                new object[] {"Carlos", "B3", "C3"},
+                new object[] {"Gordon", "B4", "C4"},
+            };
+
+            var dataRight = new[]
+            {
+                new object[] {"Carlos", "D1"},
+                new object[] {"Tatiana", "D2"},
+                new object[] {"Henry", "D3"},
+                new object[] {"Gordon", "D4"},
+            };
+
+            var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
+            var tableRight = new Table(Logger, Display, columnsRight, "D", dataRight);
+
+            var joinedTable = tableLeft.Join(tableRight);
+
+            var expectedData = new[]
+            {
+                new object[] {"Carlos", "B1", "C1", "Carlos", "D1"},
+                new object[] {"Tatiana", "B2", "C2", "Tatiana", "D2"},
+                new object[] {"Carlos", "B3", "C3", "Carlos", "D1"},
+                new object[] {"Gordon", "B4", "C4", "Gordon", "D4"},
+            };
+
+            var expectedColumns = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            Assert.AreEqual(expectedData, joinedTable.Data);
+            Assert.AreEqual(expectedColumns, joinedTable.Columns);
+        }
+
+        [Test]
+        public void JoinWithMultipleInLeftAndRightTable()
+        {
+            var columnsLeft = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+            };
+
+            var columnsRight = new List<string>
+            {
+                "D",
+                "E",
+            };
+
+            var dataLeft = new[]
+            {
+                new object[] {"Carlos", "B1", "C1"},
+                new object[] {"Tatiana", "B2", "C2"},
+                new object[] {"Carlos", "B3", "C3"},
+                new object[] {"Gordon", "B4", "C4"},
+            };
+
+            var dataRight = new[]
+            {
+                new object[] {"Carlos", "D1"},
+                new object[] {"Tatiana", "D2"},
+                new object[] {"Carlos", "D3"},
+                new object[] {"Gordon", "D4"},
+            };
+
+            var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
+            var tableRight = new Table(Logger, Display, columnsRight, "D", dataRight);
+
+            var joinedTable = tableLeft.Join(tableRight);
+
+            var expectedData = new[]
+            {
+                new object[] {"Carlos", "B1", "C1", "Carlos", "D1"},
+                new object[] {"Carlos", "B1", "C1", "Carlos", "D3"},
+                new object[] {"Tatiana", "B2", "C2", "Tatiana", "D2"},
+                new object[] {"Carlos", "B3", "C3", "Carlos", "D1"},
+                new object[] {"Carlos", "B3", "C3", "Carlos", "D3"},
+                new object[] {"Gordon", "B4", "C4", "Gordon", "D4"},
+            };
+
+            var expectedColumns = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            Assert.AreEqual(expectedData, joinedTable.Data);
+            Assert.AreEqual(expectedColumns, joinedTable.Columns);
+        }
+
+        [Test]
+        public void JoinEmptyLeftTable()
+        {
+            var columnsLeft = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+            };
+
+            var columnsRight = new List<string>
+            {
+                "D",
+                "E",
+            };
+
+            var dataLeft = new object[][] { };
+
+            var dataRight = new[]
+            {
+                new object[] {"Carlos", "D1"},
+                new object[] {"Tatiana", "D2"},
+                new object[] {"Henry", "D3"},
+            };
+
+            var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
+            var tableRight = new Table(Logger, Display, columnsRight, "D", dataRight);
+
+            var joinedTable = tableLeft.Join(tableRight);
+
+            var expectedData = new object[][] { };
+
+            var expectedColumns = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            Assert.AreEqual(expectedData, joinedTable.Data);
+            Assert.AreEqual(expectedColumns, joinedTable.Columns);
+        }
+
+        [Test]
+        public void JoinEmptyRightTable()
+        {
+            var columnsLeft = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+            };
+
+            var columnsRight = new List<string>
+            {
+                "D",
+                "E",
+            };
+
+            var dataLeft = new[]
+            {
+                new object[] {"Carlos", "B1", "C1"},
+                new object[] {"Tatiana", "B2", "C2"},
+                new object[] {"Henry", "B3", "C3"},
+            };
+
+            var dataRight = new object[][] { };
+
+            var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
+            var tableRight = new Table(Logger, Display, columnsRight, "D", dataRight);
+
+            var joinedTable = tableLeft.Join(tableRight);
+
+            var expectedData = new object[][] { };
+
+            var expectedColumns = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E"
+            };
+
+            Assert.AreEqual(expectedData, joinedTable.Data);
+            Assert.AreEqual(expectedColumns, joinedTable.Columns);
+        }
+        
+        [Test]
+        public void JoinNoMatch()
+        {
+            var columnsLeft = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+            };
+
+            var columnsRight = new List<string>
+            {
+                "D",
+                "E",
+            };
+
+            var dataLeft = new[]
+            {
+                new object[] {"Carlos", "B1", "C1"},
+                new object[] {"Tatiana", "B2", "C2"},
+            };
+
+            var dataRight = new[]
+            {
+                new object[] {"Henry", "D3"},
+                new object[] {"Gordon", "D4"},
+            };
+            
+            var tableLeft = new Table(Logger, Display, columnsLeft, "A", dataLeft);
+            var tableRight = new Table(Logger, Display, columnsRight, "D", dataRight);
+
+            var joinedTable = tableLeft.Join(tableRight);
+
+            var expectedData = new object[][] { };
 
             var expectedColumns = new List<string>
             {
